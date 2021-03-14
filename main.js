4d986b54ec8eeb931e29593e8a8b504518385394
Liseon <blzl.l.gtsj@gmail.com>
2021-03-14 12:25:17 +0800

    Done with games page design

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
-async function funfact() {
-    const factData = await fetch ('https://uselessfacts.jsph.pl/random.json?language=en');
-    const factJSON = await factData.json()
-    console.log(factJSON.text)
-    document.getElementById("fun_fact").innerHTML = "Did You Know: " + factJSON.text.toString();
+//code for main page
+function funfact() {
+    fetch ('https://uselessfacts.jsph.pl/random.json?language=en')
+    .then((res) => res.json())
+    .then((data) => {
+        document.getElementById("fun_fact").innerHTML = "Did You Know: " + data.text.toString();
+    });
 }
-funfact()
+
+//code for games page
+function gamesList() {
+    const app = document.getElementById("games")
+    const container = document.createElement('div');
+    container.setAttribute('class', 'container');
+    app.appendChild(container)
+    let gamesList = ["Blackjack", "Baccarat", "Minesweeper", "2048", "Tic-tac-toe", "Reversi", "Sudoku", 'Coin flipping', 'Rock paper scissors']
+    gamesList.forEach(games => {
+        fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${games}`)
+        .then((res) => res.json())
+        .then((data) => {
+
+            const json = data.query.search[0];
+            const url = `https://en.wikipedia.org/?curid=${json.pageid}`;
+            const descSnippet = `${json.snippet}...`;
+
+            container.insertAdjacentHTML(
+                'beforeend',
+                `<div class="card" onclick="location.href='index.html'">
+                    <h2><a href="${url}">${games}</a></h2>
+                    <p>${descSnippet}</p>
+                </div>`
+            )
+        });
+    })
+}
+//code for covid page
